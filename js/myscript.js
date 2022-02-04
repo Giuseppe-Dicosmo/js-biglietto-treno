12087f68750525b7cb2cc0c4a6cd4bad7f9b0ad6
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-02-04 21:17:59 +0100

    .

--- js/myscript.js
+++ js/myscript.js
@@ -2,37 +2,41 @@
 let quantiKm = parseFloat(prompt('Ciao, puoi dirmi quanti chilometri dista La tua destinazione'));
 console.log(quantiKm);
 
-if (isNaN(quantiKm)) {
-  alert('we frate hai sbagliato, inserisci numero')
-} else {
-  alert('bravo hai messo un numero')
-}
-
 // Prezzo del biglietto in base alla distanza
 let prezzoDelBiglietto = parseFloat(quantiKm * 0.21);
 console.log(prezzoDelBiglietto);
 
 // Età dell'utente.
-let quantiAnni = parseFloat(prompt('Quanti anni hai?'));
-console.log(quantiAnni);
-
-if (isNaN(quantiAnni)) {
+if (isNaN(quantiKm)) {
   alert('we frate hai sbagliato, inserisci numero')
 } else {
-  alert('bravo hai messo un numero')
+  let quantiAnni = parseFloat(prompt('Quanti anni hai?'));
+  console.log(quantiAnni);
+
+  if (isNaN(quantiAnni)) {
+    alert('we frate hai sbagliato, inserisci numero')
+  } else {
+    //Sconti in base all'età.
+    if (quantiAnni < 18) {
+      //Sconto per i minorenni
+      let minorenne = parseFloat(prezzoDelBiglietto * 0.80);
+      console.log(minorenne);
+    } else if(quantiAnni >= 65) {
+      //Sconto per anziani
+      let anziano = parseFloat(prezzoDelBiglietto * 0.60);
+      console.log(anziano);
+    } else {
+      //Prezzo per adulti
+      let prezzoDelBiglietto = parseFloat(quantiKm * 0.21);
+      console.log(prezzoDelBiglietto);
+    }
+  }
 }
 
-//Sconti in base all'età.
-if (quantiAnni < 18) {
-  //Sconto per i minorenni
-  let minorenne = parseFloat(prezzoDelBiglietto * 0.80);
-  console.log(minorenne);
-} else if(quantiAnni >= 65) {
-  //Sconto per anziani
-  let anziano = parseFloat(prezzoDelBiglietto * 0.60);
-  console.log(anziano);
-} else {
-  //Prezzo per adulti
-  let prezzoDelBiglietto = parseFloat(quantiKm * 0.21);
-  console.log(prezzoDelBiglietto);
-}
+
+
+// if (isNaN(quantiKm)) {
+//   alert('we frate hai sbagliato, inserisci numero')
+// } else {
+//   alert('bravo hai messo un numero')
+// }
