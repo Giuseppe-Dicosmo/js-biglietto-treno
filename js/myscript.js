5a20d9f0fc49b765159ff5b53df2fd5fdb56d9a8
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-02-04 20:40:32 +0100

    .

--- js/myscript.js
+++ js/myscript.js
@@ -2,6 +2,12 @@
 let quantiKm = parseFloat(prompt('Ciao, puoi dirmi quanti chilometri dista La tua destinazione'));
 console.log(quantiKm);
 
+if (isNaN(quantiKm)) {
+  alert('we frate hai sbagliato, inserisci numero')
+} else {
+  alert('bravo hai messo un numero')
+}
+
 // Prezzo del biglietto in base alla distanza
 let prezzoDelBiglietto = parseFloat(quantiKm * 0.21);
 console.log(prezzoDelBiglietto);
@@ -10,25 +16,23 @@ console.log(prezzoDelBiglietto);
 let quantiAnni = parseFloat(prompt('Quanti anni hai?'));
 console.log(quantiAnni);
 
+if (isNaN(quantiAnni)) {
+  alert('we frate hai sbagliato, inserisci numero')
+} else {
+  alert('bravo hai messo un numero')
+}
+
 //Sconti in base all'età.
-if ((quantiAnni < 18) && (quantiAnni >= 65)) {
+if (quantiAnni < 18) {
   //Sconto per i minorenni
   let minorenne = parseFloat(prezzoDelBiglietto * 0.80);
   console.log(minorenne);
-
+} else if(quantiAnni >= 65) {
   //Sconto per anziani
   let anziano = parseFloat(prezzoDelBiglietto * 0.60);
   console.log(anziano);
-
 } else {
   //Prezzo per adulti
   let prezzoDelBiglietto = parseFloat(quantiKm * 0.21);
   console.log(prezzoDelBiglietto);
 }
-
-
-// if (isNaN(quantiKm)) {
-//   alert('we frate hai sbagliato, inserisci numero')
-// } else {
-//   alert('bravo hai messo un numero')
-// }
