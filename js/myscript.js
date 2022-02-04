00a2aa51b36cd9d0031c12e9355629e4a6db6368
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-02-04 16:52:17 +0100

    Prezzo del biglietto in base alla distanza

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,15 @@
 // Chiede quanti chilometri deve fare l'utente
-const quantiKm = parseInt( prompt('Ciao, puoi dirmi quanti chilometri dista La tua destinazione') );
-console.log(typeof quantiKm);
+let quantiKm = parseFloat(prompt('Ciao, puoi dirmi quanti chilometri dista La tua destinazione'));
+console.log(quantiKm);
+
+// Prezzo del biglietto in base alla distanza
+let prezzoDelBiglietto = Number(quantiKm * 0.21);
+console.log(prezzoDelBiglietto);
+
+
+
+// if (isNaN(quantiKm)) {
+//   alert('we frate hai sbagliato, inserisci numero')
+// } else {
+//   alert('bravo hai messo un numero')
+// }
